487d5e2931c88f2ef546d8e96c13fdd7d23289e3
jccastellanos <jccastellanos@grupoasd.com.co>
2021-10-05 16:53:04 -0500

    Adicion react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Ventanilla</Link>
+          </li>
+          <li>
+            <Link to="/tramites">Estado de trámites</Link>
+          </li>
+          <li>
+            <Link to="/autorizacion-titulos">Autorización titulos</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="App">
+          <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
